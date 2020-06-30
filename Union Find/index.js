7ba1de53e167fb60dbbdcfd17a27926908823ac1
Johnbel <johnbel@Johnbels-MacBook-Air.local>
2020-06-30 15:35:56 -0400

    error not connecting

---
+++ Union Find/index.js
@@ -0,0 +1,27 @@
+var array = [9,48,38,85,88];
+var array01 = [9,48,38,85,88];
+  // Iterates over array elements
+// console.log(array);
+// console.log(array01);
+
+for (var i = 0; i < array.length;i++){
+for (var st = 0; st < array.length;st++)
+{
+{
+//  array+=array[i];
+ console.log(array[i])
+
+}
+
+console.log("-----------------------------------");
+for (var st = 0; st < array.length;st++)
+{
+//  array+=array[i];
+ console.log(array01[st])
+
+}
+if (array[i]===array01[st])
+
+{
+console.log(array[i]===array01[st])
+}
